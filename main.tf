90d725cc6610c9ca96c0b883dbf5a1bfde4813e5
jjliggett <67353173+jjliggett@users.noreply.github.com>
2023-05-08 03:24:32 +0000

    feat: Add two more jjversion repos

--- main.tf
+++ main.tf
@@ -50,6 +50,67 @@ resource "github_repository" "jjversion" {
   }
 }
 
+resource "github_repository" "jjversion-action" {
+  name                   = "jjversion-action"
+  description            = "A git repo SemVer versioning composite GitHub Action for Ubuntu, Windows, and MacOS, using github.com/jjliggett/jjversion"
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_rebase_merge     = true
+  delete_branch_on_merge = true
+  archive_on_destroy     = true
+  has_discussions        = true
+  has_downloads          = true
+  has_issues             = true
+  has_projects           = true
+  has_wiki               = true
+  vulnerability_alerts   = true
+  topics = [
+    "git",
+    "golang",
+    "devops",
+    "semver",
+    "versioning",
+    "hacktoberfest",
+    "github-actions",
+    "github-action",
+    "versioning-workflow"
+  ]
+  security_and_analysis {
+    secret_scanning {
+      status = "enabled"
+    }
+    secret_scanning_push_protection {
+      status = "enabled"
+    }
+  }
+}
+
+resource "github_repository" "jjversion-gha-output" {
+  name                   = "jjversion-gha-output"
+  description            = "A versioning app built specifically for the GitHub Action jjliggett/jjversion-action. For an executable for your machine, see jjliggett/jjversion"
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_rebase_merge     = true
+  delete_branch_on_merge = true
+  archive_on_destroy     = true
+  has_downloads          = true
+  has_issues             = true
+  has_projects           = true
+  has_wiki               = true
+  vulnerability_alerts   = true
+  topics = [
+    "hacktoberfest"
+  ]
+  security_and_analysis {
+    secret_scanning {
+      status = "enabled"
+    }
+    secret_scanning_push_protection {
+      status = "enabled"
+    }
+  }
+}
+
 resource "github_repository" "CurrentTimeApp" {
   name                   = "CurrentTimeApp"
   description            = "A .NET 7 Blazor WASM and MAUI Blazor application that displays the time and the next time change."
@@ -88,7 +149,7 @@ resource "github_repository" "CurrentTimeApp" {
 }
 
 locals {
-  repositories = toset(["tf-github-config", "jjversion", "CurrentTimeApp"])
+  repositories = toset(["tf-github-config", "jjversion", "jjversion-action", "jjversion-gha-output", "CurrentTimeApp"])
 }
 
 resource "github_branch_default" "tf-github-branch-default" {
